b2fe28158a8e49f82daa291d480b7b7d6cd879a6
lihj <hongji.li@alcatel-lucent.com>
2016-07-18 15:54:38 +0800

    update api

--- src/com/nsb/enms/restful/server/ne/PostNe.java
+++ src/com/nsb/enms/restful/server/ne/PostNe.java
@@ -1,47 +1,17 @@
 package com.nsb.enms.restful.server.ne;
 
-import java.util.List;
-
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.util.JSON;
-import com.nsb.enms.mongodb.constant.DBConst;
-import com.nsb.enms.mongodb.mgr.MongoDBMgr;
-
 @Path("")
-public class PostNe
-{
-    MongoDatabase db = MongoDBMgr.getInstance().getDatabase();
+public class PostNe {
 
-    MongoCollection<BasicDBObject> dbc = db.getCollection( DBConst.DB_NAME_NE,
-        BasicDBObject.class );
-
-    @POST
-    @Path("/ne")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void addNe( String ne )
-    {
-        System.out.println( ne );
-        BasicDBObject obj = (BasicDBObject) JSON.parse( ne );
-        BasicDBObject newObj = (BasicDBObject) obj.get( "NE" );
-        dbc.insertOne( newObj );
-    }
-
-    @POST
-    @Path("/nes")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void addNes( String nes )
-    {
-        System.out.println( nes );
-        BasicDBObject obj = (BasicDBObject) JSON.parse( nes );
-        @SuppressWarnings("unchecked")
-        List<BasicDBObject> newObj = (List<BasicDBObject>) obj.get( "NE" );
-        dbc.insertMany( newObj );
-    }
+	@POST
+	@Path("/nes")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void addNes(String nes) {
+		System.out.println(nes);
+	}
 }
