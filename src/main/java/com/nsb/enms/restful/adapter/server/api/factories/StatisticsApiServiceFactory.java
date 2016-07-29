2cba282bb8ca4d60559e534b305ab1a799ec0bf2
lihj <hongji.li@alcatel-lucent.com>
2016-07-29 17:36:11 +0800

    modify method

--- src/main/java/com/nsb/enms/restful/adapter/server/api/factories/StatisticsApiServiceFactory.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/factories/StatisticsApiServiceFactory.java
@@ -3,7 +3,7 @@ package com.nsb.enms.restful.adapter.server.api.factories;
 import com.nsb.enms.restful.adapter.server.api.StatisticsApiService;
 import com.nsb.enms.restful.adapter.server.api.impl.StatisticsApiServiceImpl;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T12:31:04.853+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T17:16:31.406+08:00")
 public class StatisticsApiServiceFactory {
     private final static StatisticsApiService service = new StatisticsApiServiceImpl();
 
--- src/main/java/com/nsb/enms/restful/adapter/server/api/impl/TpsApiServiceImpl.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/impl/TpsApiServiceImpl.java
@@ -16,7 +16,7 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T12:31:04.853+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T17:16:31.406+08:00")
 public class TpsApiServiceImpl extends TpsApiService {
     @Override
     public Response addTPs(List<TP> body, SecurityContext securityContext) throws NotFoundException {
