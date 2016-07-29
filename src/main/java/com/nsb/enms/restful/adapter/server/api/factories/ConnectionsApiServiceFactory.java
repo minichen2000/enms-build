5fae224d718efdbed5b681c62c5f6c07bfdfb8fa
Cui Kai <kai.cui@alcatel-lucent.com>
2016-07-29 03:45:04 -0400

    reset to remove DJM's code

--- src/main/java/com/nsb/enms/restful/adapter/server/api/factories/ConnectionsApiServiceFactory.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/factories/ConnectionsApiServiceFactory.java
@@ -3,7 +3,7 @@ package com.nsb.enms.restful.adapter.server.api.factories;
 import com.nsb.enms.restful.adapter.server.api.ConnectionsApiService;
 import com.nsb.enms.restful.adapter.server.api.impl.ConnectionsApiServiceImpl;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T11:07:52.849+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T12:31:04.853+08:00")
 public class ConnectionsApiServiceFactory {
     private final static ConnectionsApiService service = new ConnectionsApiServiceImpl();
 
--- src/main/java/com/nsb/enms/restful/adapter/server/api/impl/RoutesApiServiceImpl.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/impl/RoutesApiServiceImpl.java
@@ -15,7 +15,7 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T11:07:52.849+08:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T12:31:04.853+08:00")
 public class RoutesApiServiceImpl extends RoutesApiService {
     @Override
     public Response routesGet(String anode, String znode, SecurityContext securityContext) throws NotFoundException {
