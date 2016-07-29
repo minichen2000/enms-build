d32fa18dba245658263d076f286b0fc480c2f8bd
lihj <hongji.li@alcatel-lucent.com>
2016-07-29 17:58:18 +0800

    update

--- src/main/java/com/nsb/enms/restful/adapter/server/business/SyncTpThread.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/business/SyncTpThread.java
@@ -1,11 +1,15 @@
 package com.nsb.enms.restful.adapter.server.business;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.nsb.enms.restful.adapter.server.action.entity.TpEntity;
 import com.nsb.enms.restful.adapter.server.action.method.ne.StartSuppervision;
 import com.nsb.enms.restful.adapter.server.action.method.tp.GetTp;
 import com.nsb.enms.restful.adapter.server.common.exception.AdapterException;
+import com.nsb.enms.restful.db.client.ApiException;
+import com.nsb.enms.restful.db.client.api.TpsApi;
+import com.nsb.enms.restful.db.client.model.TP;
 
 public class SyncTpThread extends Thread {
 
@@ -22,18 +26,29 @@ public class SyncTpThread extends Thread {
 		boolean isSuccess;
 		try {
 			isSuccess = start.startSuppervision(groupId, neId);
-			if (isSuccess) {
-				GetTp getTp = new GetTp();
-				try {
-					List<TpEntity> tpList = getTp.getTp(groupId, neId);
-					
-					// TODO insert to db
-				} catch (AdapterException e) {
-					e.printStackTrace();
-				}
+			if (!isSuccess) {
+				return;
 			}
-		} catch (AdapterException e1) {
-			e1.printStackTrace();
+		} catch (AdapterException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		GetTp getTp = new GetTp();
+		try {
+			List<TpEntity> tpList = getTp.getTp(groupId, neId);
+			List<TP> tps = new ArrayList<TP>();
+			TpsApi tpsApi = new TpsApi();
+			for (TpEntity tp : tpList) {
+				TP newTp = new TP();
+				newTp.setUserLabel(tp.getUserLabel());
+				tps.add(newTp);
+			}
+			tpsApi.addTPs(tps);
+		} catch (AdapterException e) {
+			e.printStackTrace();
+		} catch (ApiException e) {
+			e.printStackTrace();
 		}
 	}
 }
