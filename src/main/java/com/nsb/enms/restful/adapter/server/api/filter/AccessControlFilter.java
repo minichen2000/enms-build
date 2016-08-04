c940751651c57d4898e6ea268de8708e77681427
lihj <hongji.li@alcatel-lucent.com>
2016-08-04 15:38:16 +0800

    add filter

---
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/filter/AccessControlFilter.java
@@ -0,0 +1,24 @@
+package com.nsb.enms.restful.adapter.server.api.filter;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccessControlFilter implements javax.servlet.Filter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		res.addHeader("Access-Control-Allow-Headers",
+				"Access-Control-Allow-Origin,Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers");
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+}
