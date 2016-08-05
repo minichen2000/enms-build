d88184e4a198fbf3f05d6bacad82047512841d3b
lihj <hongji.li@alcatel-lucent.com>
2016-08-05 17:35:26 +0800

    add method

--- src/main/java/com/nsb/enms/restful/adapter/server/api/impl/StatisticsApiServiceImpl.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/impl/StatisticsApiServiceImpl.java
@@ -1,30 +1,42 @@
 package com.nsb.enms.restful.adapter.server.api.impl;
 
-import com.nsb.enms.restful.adapter.server.api.*;
-import com.nsb.enms.restful.adapter.server.model.*;
-
-import com.nsb.enms.restful.adapter.server.model.NESTATISTICS;
-
+import java.util.ArrayList;
 import java.util.List;
-import com.nsb.enms.restful.adapter.server.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
+import com.nsb.enms.restful.adapter.server.api.NotFoundException;
+import com.nsb.enms.restful.adapter.server.api.StatisticsApiService;
+import com.nsb.enms.restful.db.client.ApiException;
+import com.nsb.enms.restful.db.client.api.StatisticsApi;
+import com.nsb.enms.restful.db.client.model.NESTATISTICS;
+
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2016-07-29T17:16:31.406+08:00")
 public class StatisticsApiServiceImpl extends StatisticsApiService {
-    @Override
-    public Response statisticsNesGet(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
-    @Override
-    public Response statisticsSupportedNeTypesGet(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
+	private StatisticsApi statisticsApi = new StatisticsApi();
+
+	@Override
+	public Response statisticsNesGet(SecurityContext securityContext) throws NotFoundException {
+		List<NESTATISTICS> neStatisticsList = new ArrayList<NESTATISTICS>();
+		try {
+			neStatisticsList = statisticsApi.statisticsNesGet();
+		} catch (ApiException e) {
+			e.printStackTrace();
+			Response.serverError().entity(e).build();
+		}
+		return Response.ok().entity(neStatisticsList).build();
+	}
+
+	@Override
+	public Response statisticsSupportedNeTypesGet(SecurityContext securityContext) throws NotFoundException {
+		List<NESTATISTICS> neStatisticsList = new ArrayList<NESTATISTICS>();
+		try {
+			neStatisticsList = statisticsApi.statisticsSupportedNeTypesGet();
+		} catch (ApiException e) {
+			e.printStackTrace();
+			Response.serverError().entity(e).build();
+		}
+		return Response.ok().entity(neStatisticsList).build();
+	}
 }
