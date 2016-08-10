b30d0cfd5dd63ffcfb7f80433422bba311b4aec8
guiqingl <lingq@chinawiserv.com>
2016-08-10 16:45:31 +0800

    update

--- src/main/java/com/nsb/enms/restful/adapter/server/api/Bootstrap.java
+++ src/main/java/com/nsb/enms/restful/adapter/server/api/Bootstrap.java
@@ -1,7 +1,5 @@
 package com.nsb.enms.restful.adapter.server.api;
 
-import java.io.File;
-import java.io.FileInputStream;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -10,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.ConfigurationSource;
-import org.apache.logging.log4j.core.config.Configurator;
 
 import com.nsb.enms.restful.adapter.server.business.register.RegisterManager;
 import com.nsb.enms.restful.adapter.server.common.conf.ConfLoader;
@@ -25,59 +21,68 @@ import io.swagger.models.Info;
 import io.swagger.models.License;
 import io.swagger.models.Swagger;
 
-public class Bootstrap extends HttpServlet {
-	private static final long serialVersionUID = 6213433092343311611L;
+public class Bootstrap extends HttpServlet
+{
+    private static final long serialVersionUID = 6213433092343311611L;
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		Info info = new Info().title("Swagger Server").description("ENMS API.  Adapter RESTful interface. ")
-				.termsOfService("").contact(new Contact().email("")).license(new License().name("").url(""));
+    private static final Logger log = LogManager.getLogger( Bootstrap.class );
 
-		ServletContext context = config.getServletContext();
-		loadConf(context);
+    @Override
+    public void init( ServletConfig config ) throws ServletException
+    {
+        Info info = new Info().title( "Swagger Server" )
+                .description( "ENMS API.  Adapter RESTful interface. " )
+                .termsOfService( "" ).contact( new Contact().email( "" ) )
+                .license( new License().name( "" ).url( "" ) );
 
-		Swagger swagger = new Swagger().info(info);
-		new SwaggerContextService().withServletConfig(config).updateSwagger(swagger);
+        ServletContext context = config.getServletContext();
+        loadConf( context );
 
-		register2Controller();
-	}
+        Swagger swagger = new Swagger().info( info );
+        new SwaggerContextService().withServletConfig( config )
+                .updateSwagger( swagger );
 
-	private void loadConf(ServletContext context) {
-		String confPath = context.getRealPath("/WEB-INF/conf");
-		try {
-			ConfLoader.getInstance().loadConf(confPath + "/conf.properties");
-		} catch (AdapterException e) {
-			e.printStackTrace();
-		}
+        register2Controller();
+    }
 
-		ConfigurationSource source;
-		try {
-			File config = new File(confPath + "/log4j2.xml");
-			source = new ConfigurationSource(new FileInputStream(config), config);
-			Configurator.initialize(null, source);
-			Logger log = LogManager.getLogger(Bootstrap.class);
-			log.debug("The confPath is " + confPath);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    private void loadConf( ServletContext context )
+    {
+        String confPath = context.getRealPath( "/WEB-INF/conf" );
+        log.debug( "The confPath is " + confPath );
+        try
+        {
+            ConfLoader.getInstance().loadConf( confPath + "/conf.properties" );
+        }
+        catch( AdapterException e )
+        {
+            e.printStackTrace();
+        }
 
-		String dbUrl = ConfLoader.getInstance().getConf("DB_URL", "");
-		initDbApiClient(dbUrl);
+        String dbUrl = ConfLoader.getInstance().getConf( "DB_URL", "" );
+        log.debug( "The dbUrl is " + dbUrl );
+        initDbApiClient( dbUrl );
 
-		String ctrlUrl = ConfLoader.getInstance().getConf("CTRL_URL", "");
-		initControllerApiClient(ctrlUrl);
-	}
+        String ctrlUrl = ConfLoader.getInstance().getConf( "CTRL_URL", "" );
+        log.debug( "The ctrlUrl is " + ctrlUrl );
+        initControllerApiClient( ctrlUrl );
+    }
 
-	private void initControllerApiClient(String ctrlUrl) {
-		com.nsb.enms.restful.controller.client.Configuration
-				.setDefaultApiClient(new com.nsb.enms.restful.controller.client.ApiClient().setBasePath(ctrlUrl));
-	}
+    private void initControllerApiClient( String ctrlUrl )
+    {
+        com.nsb.enms.restful.controller.client.Configuration
+                .setDefaultApiClient(
+                    new com.nsb.enms.restful.controller.client.ApiClient()
+                            .setBasePath( ctrlUrl ) );
+    }
 
-	private void initDbApiClient(String dbUrl) {
-		Configuration.setDefaultApiClient(new ApiClient().setBasePath(dbUrl));
-	}
+    private void initDbApiClient( String dbUrl )
+    {
+        Configuration
+                .setDefaultApiClient( new ApiClient().setBasePath( dbUrl ) );
+    }
 
-	private void register2Controller() {
-		new RegisterManager().register2Controller();
-	}
+    private void register2Controller()
+    {
+        new RegisterManager().register2Controller();
+    }
 }
