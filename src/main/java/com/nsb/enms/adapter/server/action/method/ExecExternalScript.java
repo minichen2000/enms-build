45610da48c32927cc0d03d63075f103e3d0b0f4f
lihj <hongji.li@alcatel-lucent.com>
2016-09-27 17:25:06 +0800

    add stub test

--- src/main/java/com/nsb/enms/adapter/server/action/method/ExecExternalScript.java
+++ src/main/java/com/nsb/enms/adapter/server/action/method/ExecExternalScript.java
@@ -12,62 +12,103 @@ import com.nsb.enms.adapter.server.common.conf.ConfLoader;
 import com.nsb.enms.adapter.server.common.conf.ConfigKey;
 import com.nsb.enms.adapter.server.common.exception.AdapterException;
 import com.nsb.enms.adapter.server.common.exception.AdapterExceptionType;
+import com.nsb.enms.common.EnmsEnv;
+import com.nsb.enms.common.utils.Env;
 
-public class ExecExternalScript
-{
-    private static final Logger log = LogManager
-            .getLogger( ExecExternalScript.class );
+public class ExecExternalScript {
+	private static final Logger log = LogManager.getLogger(ExecExternalScript.class);
 
-    private static String tstmgrScript = ConfLoader.getInstance().getConf(
-        ConfigKey.TSTMGR_SCRIPT, ConfigKey.DEFAULT_TSTMGR_SCRIPT );
-    
-   /* private static String q3EmlImScript = ConfLoader.getInstance().getConf(
-        ConfigKey.START_Q3_EMLIM_SCRIPT, ConfigKey.DEFAULT_START_Q3_EMLIM_SCRIPT );*/
+	private static String tstmgrScript = ConfLoader.getInstance().getConf(ConfigKey.TSTMGR_SCRIPT,
+			ConfigKey.DEFAULT_TSTMGR_SCRIPT);
 
-    private static String tstmgrDir = ConfLoader.getInstance().getConf(
-        ConfigKey.TSTMGR_SCRIPT_DIR, ConfigKey.DEFAULT_TSTMGR_SCRIPT_DIR );
+	/*
+	 * private static String q3EmlImScript = ConfLoader.getInstance().getConf(
+	 * ConfigKey.START_Q3_EMLIM_SCRIPT, ConfigKey.DEFAULT_START_Q3_EMLIM_SCRIPT
+	 * );
+	 */
 
-    /*private static String emlImDir = ConfLoader.getInstance().getConf(
-        ConfigKey.Q3_EMLIM_SCRIPT_DIR, ConfigKey.DEFAULT_Q3_EMLIM_SCRIPT_DIR );
-    
-    private static String killEmlImScript = ConfLoader.getInstance().getConf(
-        ConfigKey.KILL_EMLIM_SCRIPT, ConfigKey.DEFAULT_KILL_EMLIM_SCRIPT );*/
+	private static String tstmgrDir = ConfLoader.getInstance().getConf(ConfigKey.TSTMGR_SCRIPT_DIR,
+			ConfigKey.DEFAULT_TSTMGR_SCRIPT_DIR);
 
-    private static File fileDir;
+	/*
+	 * private static String emlImDir = ConfLoader.getInstance().getConf(
+	 * ConfigKey.Q3_EMLIM_SCRIPT_DIR, ConfigKey.DEFAULT_Q3_EMLIM_SCRIPT_DIR );
+	 * 
+	 * private static String killEmlImScript = ConfLoader.getInstance().getConf(
+	 * ConfigKey.KILL_EMLIM_SCRIPT, ConfigKey.DEFAULT_KILL_EMLIM_SCRIPT );
+	 */
 
-    public static Process run( ExternalScriptType scriptType, String... params ) throws AdapterException
-    {
-        String[] cmdArray = new String[params.length + 1];
-        switch( scriptType )
-        {
-            case TSTMGR:
-                fileDir = new File( tstmgrDir );
-                cmdArray[0] = tstmgrScript;
-                break;
-            /*case EMLIM:
-                fileDir = new File( emlImDir );
-                cmdArray[0] = q3EmlImScript;
-                break;
-            case KILL_EMLIM:
-                fileDir = new File( tstmgrDir );
-                cmdArray[0] = killEmlImScript;
-                break;*/
-            default:
-                break;
-        }
-        
-        System.arraycopy( params, 0, cmdArray, 1, params.length );
-        log.debug( "exec:" + Arrays.asList( cmdArray ) );
+	private static File fileDir;
 
-        try
-        {
-            return Runtime.getRuntime().exec( cmdArray, null, fileDir );
-        }
-        catch( IOException e )
-        {
-            log.error( "execExternalScript", e );
-            throw new AdapterException(
-                    AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage() );
-        }
-    }
+	public static Process run(ExternalScriptType scriptType, String... params) throws AdapterException {
+		Env.checkEnvironment();
+		if (EnmsEnv.ENMS_STUB_MODE) {
+			return run4Stub(scriptType, params);
+		} else {
+			return run4Normal(scriptType, params);
+		}
+	}
+
+	private static Process run4Normal(ExternalScriptType scriptType, String... params) throws AdapterException {
+		String[] cmdArray = new String[params.length + 1];
+		switch (scriptType) {
+		case TSTMGR:
+			fileDir = new File(tstmgrDir);
+			cmdArray[0] = tstmgrScript;
+			break;
+		/*
+		 * case EMLIM: fileDir = new File( emlImDir ); cmdArray[0] =
+		 * q3EmlImScript; break; case KILL_EMLIM: fileDir = new File( tstmgrDir
+		 * ); cmdArray[0] = killEmlImScript; break;
+		 */
+		default:
+			break;
+		}
+
+		System.arraycopy(params, 0, cmdArray, 1, params.length);
+		log.debug("exec:" + Arrays.asList(cmdArray));
+
+		try {
+			return Runtime.getRuntime().exec(cmdArray, null, fileDir);
+		} catch (IOException e) {
+			log.error("execExternalScript", e);
+			throw new AdapterException(AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage());
+		}
+	}
+
+	private static Process run4Stub(ExternalScriptType scriptType, String... params) throws AdapterException {
+		String[] cmdArray = new String[3];
+		switch (scriptType) {
+		case TSTMGR:
+			cmdArray[0] = "python";
+			cmdArray[1] = "emlim.py";
+			break;
+		default:
+			break;
+		}
+
+		String cmd = params[0];
+		log.debug("cmd:" + cmd);
+		cmdArray[2] = cmd;
+
+		try {
+			File stubPath = new File(ConfLoader.getInstance().getConf("EMLIM_STUB_PATH", ""));
+			return Runtime.getRuntime().exec(cmdArray, null, stubPath);
+		} catch (IOException e) {
+			log.error("execExternalScript", e);
+			throw new AdapterException(AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage());
+		}
+	}
+
+	public static void main(String args[]) {
+		String os = System.getProperty("os.name");
+		System.out.println(os);
+		String cmdArray[] = { "python", "emlim.py", "/opt/nms/EML_1/TSTMGR/scenario/CreateNe.req" };
+		File stubPath = new File("D:\\workspace6\\eNMS\\enms-test\\stub\\emlim.stub");
+		try {
+			Runtime.getRuntime().exec(cmdArray, null, stubPath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
