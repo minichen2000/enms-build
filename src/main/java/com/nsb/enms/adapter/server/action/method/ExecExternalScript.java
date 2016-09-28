6efb37f2dae4e3ef40d5c87a0dfc30abd6e159fa
guiqingl <lingq@chinawiserv.com>
2016-09-28 10:06:16 +0800

    update

--- src/main/java/com/nsb/enms/adapter/server/action/method/ExecExternalScript.java
+++ src/main/java/com/nsb/enms/adapter/server/action/method/ExecExternalScript.java
@@ -15,100 +15,167 @@ import com.nsb.enms.adapter.server.common.exception.AdapterExceptionType;
 import com.nsb.enms.common.EnmsEnv;
 import com.nsb.enms.common.utils.Env;
 
-public class ExecExternalScript {
-	private static final Logger log = LogManager.getLogger(ExecExternalScript.class);
+public class ExecExternalScript
+{
+    private static final Logger log = LogManager
+            .getLogger( ExecExternalScript.class );
 
-	private static String tstmgrScript = ConfLoader.getInstance().getConf(ConfigKey.TSTMGR_SCRIPT,
-			ConfigKey.DEFAULT_TSTMGR_SCRIPT);
+    private static String tstmgrScript = ConfLoader.getInstance().getConf(
+        ConfigKey.TSTMGR_SCRIPT, ConfigKey.DEFAULT_TSTMGR_SCRIPT );
 
-	/*
-	 * private static String q3EmlImScript = ConfLoader.getInstance().getConf(
-	 * ConfigKey.START_Q3_EMLIM_SCRIPT, ConfigKey.DEFAULT_START_Q3_EMLIM_SCRIPT
-	 * );
-	 */
+    /*
+     * private static String q3EmlImScript = ConfLoader.getInstance().getConf(
+     * ConfigKey.START_Q3_EMLIM_SCRIPT, ConfigKey.DEFAULT_START_Q3_EMLIM_SCRIPT
+     * );
+     */
 
-	private static String tstmgrDir = ConfLoader.getInstance().getConf(ConfigKey.TSTMGR_SCRIPT_DIR,
-			ConfigKey.DEFAULT_TSTMGR_SCRIPT_DIR);
+    private static String tstmgrDir = ConfLoader.getInstance().getConf(
+        ConfigKey.TSTMGR_SCRIPT_DIR, ConfigKey.DEFAULT_TSTMGR_SCRIPT_DIR );
 
-	/*
-	 * private static String emlImDir = ConfLoader.getInstance().getConf(
-	 * ConfigKey.Q3_EMLIM_SCRIPT_DIR, ConfigKey.DEFAULT_Q3_EMLIM_SCRIPT_DIR );
-	 * 
-	 * private static String killEmlImScript = ConfLoader.getInstance().getConf(
-	 * ConfigKey.KILL_EMLIM_SCRIPT, ConfigKey.DEFAULT_KILL_EMLIM_SCRIPT );
-	 */
+    /*
+     * private static String emlImDir = ConfLoader.getInstance().getConf(
+     * ConfigKey.Q3_EMLIM_SCRIPT_DIR, ConfigKey.DEFAULT_Q3_EMLIM_SCRIPT_DIR );
+     * 
+     * private static String killEmlImScript = ConfLoader.getInstance().getConf(
+     * ConfigKey.KILL_EMLIM_SCRIPT, ConfigKey.DEFAULT_KILL_EMLIM_SCRIPT );
+     */
 
-	private static File fileDir;
+    private static File fileDir;
 
-	public static Process run(ExternalScriptType scriptType, String... params) throws AdapterException {
-		Env.checkEnvironment();
-		if (EnmsEnv.ENMS_STUB_MODE) {
-			return run4Stub(scriptType, params);
-		} else {
-			return run4Normal(scriptType, params);
-		}
-	}
+    public static Process run( ExternalScriptType scriptType, String... params )
+            throws AdapterException
+    {
+        Env.checkEnvironment();
+        if( EnmsEnv.ENMS_STUB_MODE )
+        {
+            return run4Stub( scriptType, params );
+        }
+        else
+        {
+            return run4Normal( scriptType, params );
+        }
+    }
 
-	private static Process run4Normal(ExternalScriptType scriptType, String... params) throws AdapterException {
-		String[] cmdArray = new String[params.length + 1];
-		switch (scriptType) {
-		case TSTMGR:
-			fileDir = new File(tstmgrDir);
-			cmdArray[0] = tstmgrScript;
-			break;
-		/*
-		 * case EMLIM: fileDir = new File( emlImDir ); cmdArray[0] =
-		 * q3EmlImScript; break; case KILL_EMLIM: fileDir = new File( tstmgrDir
-		 * ); cmdArray[0] = killEmlImScript; break;
-		 */
-		default:
-			break;
-		}
+    private static Process run4Normal( ExternalScriptType scriptType,
+            String... params ) throws AdapterException
+    {
+        String[] cmdArray = new String[params.length + 1];
+        switch( scriptType )
+        {
+            case TSTMGR:
+                fileDir = new File( tstmgrDir );
+                cmdArray[0] = tstmgrScript;
+                break;
+            /*
+             * case EMLIM: fileDir = new File( emlImDir ); cmdArray[0] =
+             * q3EmlImScript; break; case KILL_EMLIM: fileDir = new File(
+             * tstmgrDir ); cmdArray[0] = killEmlImScript; break;
+             */
+            default:
+                break;
+        }
 
-		System.arraycopy(params, 0, cmdArray, 1, params.length);
-		log.debug("exec:" + Arrays.asList(cmdArray));
+        System.arraycopy( params, 0, cmdArray, 1, params.length );
+        log.debug( "exec:" + Arrays.asList( cmdArray ) );
 
-		try {
-			return Runtime.getRuntime().exec(cmdArray, null, fileDir);
-		} catch (IOException e) {
-			log.error("execExternalScript", e);
-			throw new AdapterException(AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage());
-		}
-	}
+        try
+        {
+            return Runtime.getRuntime().exec( cmdArray, null, fileDir );
+        }
+        catch( IOException e )
+        {
+            log.error( "execExternalScript", e );
+            throw new AdapterException(
+                    AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage() );
+        }
+    }
 
-	private static Process run4Stub(ExternalScriptType scriptType, String... params) throws AdapterException {
-		String[] cmdArray = new String[3];
-		switch (scriptType) {
-		case TSTMGR:
-			cmdArray[0] = "python";
-			cmdArray[1] = "emlim.py";
-			break;
-		default:
-			break;
-		}
+    private static Process run4Stub( ExternalScriptType scriptType,
+            String... params ) throws AdapterException
+    {
+        String[] cmdArray = new String[3];
+        switch( scriptType )
+        {
+            case TSTMGR:
+                cmdArray[0] = "python";
+                cmdArray[1] = "emlim.py";
+                break;
+            default:
+                break;
+        }
 
-		String cmd = params[0];
-		log.debug("cmd:" + cmd);
-		cmdArray[2] = cmd;
+        String cmd = params[0];
+        log.debug( "cmd:" + cmd );
+        if( cmd.endsWith( "syncCTP.req" ) )
+        {
+            String objectClass = params[1];
+            if( objectClass.equals( "0.0.7.774.127.7.0.3.4" ) )
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getRsCtp" );
+            }
+            else if( objectClass.equals( "0.0.7.774.0.3.40" ) )
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getMsCtp" );
+            }
+            else if( objectClass.equals( "0.0.7.774.0.3.25" ) )
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getUnProtectedCtp" );
+            }
+            else if(objectClass.equals( "0.0.7.774.127.3.0.3.2" ))
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getAu4Ctp" );
+            }
+            else if(objectClass.equals( "1.3.12.2.1006.54.0.0.3.210" )) 
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getEPdhCtp" );
+            }
+            else if(objectClass.equals( "0.0.7.774.127.7.0.3.10" )) 
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getVc12Ttp" );
+            }
+            else if(objectClass.equals( "0.0.7.774.0.3.101" )) 
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getVc3Ttp" );
+            }
+            else if(objectClass.equals( "0.0.7.774.127.2.0.3.25" )) 
+            {
+                cmdArray[2] = cmd.replace( "syncCTP", "getTu12Ctp" );
+            }
+        }
+        else
+        {
+            cmdArray[2] = cmd;
+        }
 
-		try {
-			File stubPath = new File(ConfLoader.getInstance().getConf("EMLIM_STUB_PATH", ""));
-			return Runtime.getRuntime().exec(cmdArray, null, stubPath);
-		} catch (IOException e) {
-			log.error("execExternalScript", e);
-			throw new AdapterException(AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage());
-		}
-	}
+        try
+        {
+            File stubPath = new File(
+                    ConfLoader.getInstance().getConf( "EMLIM_STUB_PATH", "" ) );
+            return Runtime.getRuntime().exec( cmdArray, null, stubPath );
+        }
+        catch( IOException e )
+        {
+            log.error( "execExternalScript", e );
+            throw new AdapterException(
+                    AdapterExceptionType.EXCPT_INTERNAL_ERROR, e.getMessage() );
+        }
+    }
 
-	public static void main(String args[]) {
-		String os = System.getProperty("os.name");
-		System.out.println(os);
-		String cmdArray[] = { "python", "emlim.py", "/opt/nms/EML_1/TSTMGR/scenario/CreateNe.req" };
-		File stubPath = new File("D:\\workspace6\\eNMS\\enms-test\\stub\\emlim.stub");
-		try {
-			Runtime.getRuntime().exec(cmdArray, null, stubPath);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main( String args[] )
+    {
+        String os = System.getProperty( "os.name" );
+        System.out.println( os );
+        String cmdArray[] = {"python", "emlim.py",
+                "/opt/nms/EML_1/TSTMGR/scenario/CreateNe.req"};
+        File stubPath = new File(
+                "D:\\workspace6\\eNMS\\enms-test\\stub\\emlim.stub" );
+        try
+        {
+            Runtime.getRuntime().exec( cmdArray, null, stubPath );
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
 }
