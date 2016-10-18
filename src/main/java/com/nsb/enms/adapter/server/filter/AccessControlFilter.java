d3711dadb04d194d3d81288c76c0aa5c7a007a59
guiqingl <lingq@chinawiserv.com>
2016-10-18 10:14:39 +0800

    add PATCH to access filter

--- src/main/java/com/nsb/enms/adapter/server/filter/AccessControlFilter.java
+++ src/main/java/com/nsb/enms/adapter/server/filter/AccessControlFilter.java
@@ -15,7 +15,7 @@ public class AccessControlFilter implements ContainerResponseFilter
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
         headers.add( "Access-Control-Allow-Origin", "*" );
-        headers.add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+        headers.add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH" );
         headers.add( "Access-Control-Allow-Headers",
             "X-Requested-With, Content-Type, Accept, Origin, access-control-allow-origin" );
     }
