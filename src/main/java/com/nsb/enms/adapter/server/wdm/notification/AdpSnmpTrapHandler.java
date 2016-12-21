375ee176a75767df0062fadadd0a6b91fa761deb
lihj <hongji.li@alcatel-lucent.com>
2016-12-21 17:17:15 +0800

    bug fix

--- src/main/java/com/nsb/enms/adapter/server/wdm/notification/AdpSnmpTrapHandler.java
+++ src/main/java/com/nsb/enms/adapter/server/wdm/notification/AdpSnmpTrapHandler.java
@@ -8,39 +8,29 @@ import org.apache.logging.log4j.Logger;
 import com.nsb.enms.adapter.server.common.exception.AdapterException;
 import com.nsb.enms.adapter.server.common.utils.NotifQueue;
 import com.nsb.enms.adapter.server.common.utils.INotifProcessor;
+import com.nsb.enms.common.utils.Pair;
 import com.nsb.enms.common.utils.snmpclient.DispatchTrap;
 
-public class AdpSnmpTrapHandler implements DispatchTrap
-{
-    private static final Logger log = LogManager
-            .getLogger( AdpSnmpTrapHandler.class );
+public class AdpSnmpTrapHandler implements DispatchTrap {
+	private static final Logger log = LogManager.getLogger(AdpSnmpTrapHandler.class);
 
-    private static NotifQueue<Map<String, String>> queue;
+	private static NotifQueue<Map<String, String>> queue;
 
-    public AdpSnmpTrapHandler()
-    {
-        queue = new NotifQueue<Map<String, String>>(
-                new INotifProcessor<Map<String, String>>()
-                {
+	public AdpSnmpTrapHandler() {
+		queue = new NotifQueue<Map<String, String>>(new INotifProcessor<Map<String, String>>() {
 
-                    @Override
-                    public void process( Map<String, String> trapValue )
-                            throws AdapterException
-                    {
-                        for( String key : trapValue.keySet() )
-                        {
-                            System.out.println( "key = " + key + ", value = "
-                                    + trapValue.get( key ) );
-                            log.debug( "key = " + key + ", value = "
-                                    + trapValue.get( key ) );
-                        }
-                    }
-                } );
-    }
+			@Override
+			public void process(Map<String, String> trapValue) throws AdapterException {
+				for (String key : trapValue.keySet()) {
+					System.out.println("key = " + key + ", value = " + trapValue.get(key));
+					log.debug("key = " + key + ", value = " + trapValue.get(key));
+				}
+			}
+		});
+	}
 
-    @Override
-    public void onTrap( Map<String, String> trapValue )
-    {
-        queue.push( trapValue );
-    }
+	@Override
+	public void onTrap(Pair<Pair<String, Integer>, Pair<String, String>> trapInfo) {
+		// queue.push(trapValue);
+	}
 }
