95b65f35ddfb8ceed947c086e233f776440ab083
guiqingl <lingq@chinawiserv.com>
2016-12-22 16:18:39 +0800

    change oid

--- src/main/java/com/nsb/enms/adapter/server/common/utils/TimeUtil.java
+++ src/main/java/com/nsb/enms/adapter/server/common/utils/TimeUtil.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TimeUtil {
 	// private static final DateFormat LOCAL_TMF_FORMAT = new
@@ -13,6 +15,8 @@ public class TimeUtil {
 	private static final DateFormat UTC_TMF_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss.0'Z'");
 
 	private static final DateFormat LABEL_TMF_FORMAT = new SimpleDateFormat("yyyyMMddHHmm");
+
+	private static Pattern pattern = Pattern.compile("(\\d+)\\s*days,\\s*(\\d+):(\\d+):(\\d+)\\.(\\d*)");
 
 	static {
 		UTC_TMF_FORMAT.setTimeZone(TimeZone.getTimeZone("Etc/Greenwich"));
@@ -57,5 +61,20 @@ public class TimeUtil {
 			return new Date().getTime();
 		}
 		return date.getTime();
+	}
+
+	public static Long getSysUpTime(String time) {
+		long sysUpTime = 0;
+		Matcher matcher = pattern.matcher(time);
+		if (matcher.matches()) {
+			int day = Integer.valueOf(matcher.group(1));
+			int hour = Integer.valueOf(matcher.group(2));
+			int min = Integer.valueOf(matcher.group(3));
+			int sec = Integer.valueOf(matcher.group(4));
+			int hsec = Integer.valueOf(matcher.group(5));
+			sysUpTime = day * 24 * 3600 * 100 + hour * 3600 * 100 + min * 60 * 100 + sec * 100 + hsec;
+		}
+
+		return sysUpTime;
 	}
 }
--- src/main/java/com/nsb/enms/adapter/server/wdm/notification/AdpSnmpTrapHandler.java
+++ src/main/java/com/nsb/enms/adapter/server/wdm/notification/AdpSnmpTrapHandler.java
@@ -1,5 +1,6 @@
 package com.nsb.enms.adapter.server.wdm.notification;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,24 +8,29 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.nsb.enms.adapter.server.common.exception.AdapterException;
-import com.nsb.enms.adapter.server.common.utils.INotifProcessor;
 import com.nsb.enms.adapter.server.common.utils.NotifQueue;
+import com.nsb.enms.adapter.server.common.utils.TimeUtil;
+import com.nsb.enms.adapter.server.common.utils.INotifProcessor;
 import com.nsb.enms.common.utils.Pair;
 import com.nsb.enms.common.utils.snmp.DispatchTrap;
 
 public class AdpSnmpTrapHandler implements DispatchTrap {
 	private static final Logger log = LogManager.getLogger(AdpSnmpTrapHandler.class);
 
-	private static NotifQueue<Map<String, String>> queue;
+	private static final int MAX_TIME = 3;
+	
+	private static NotifQueue<List<Pair<String, String>>> queue;
+
+	private static Map<String, Long> map = new HashMap<String, Long>();
 
 	public AdpSnmpTrapHandler() {
-		queue = new NotifQueue<Map<String, String>>(new INotifProcessor<Map<String, String>>() {
+		queue = new NotifQueue<List<Pair<String, String>>>(new INotifProcessor<List<Pair<String, String>>>() {
 
 			@Override
-			public void process(Map<String, String> trapValue) throws AdapterException {
-				for (String key : trapValue.keySet()) {
-					System.out.println("key = " + key + ", value = " + trapValue.get(key));
-					log.debug("key = " + key + ", value = " + trapValue.get(key));
+			public void process(List<Pair<String, String>> trapValue) throws AdapterException {
+				for (Pair<String, String> pair : trapValue) {
+					System.out.println(pair.getFirst() + " = " + pair.getSecond());
+					log.debug(pair.getFirst() + " = " + pair.getSecond());
 				}
 			}
 		});
@@ -32,6 +38,47 @@ public class AdpSnmpTrapHandler implements DispatchTrap {
 
 	@Override
 	public void onTrap(List<Pair<String, String>> trapInfo) {
+		long timeStamp = 0;
+		boolean flag = false;
+		String ip = "";
+		String objectId = "";
+		String data = "";
+		String changeObject = "";
+		for (Pair<String, String> pair : trapInfo) {
+			if (pair.getFirst().startsWith("1.3.6.1.4.1.7483.2.1.2.2.1.1.1.3")) {
+				timeStamp = TimeUtil.getSysUpTime(pair.getSecond());
+				continue;
+			}
 
+			if (pair.getFirst().startsWith("1.3.6.1.4.1.7483.2.1.2.2.1.1.1.5")) {
+				objectId = pair.getSecond();
+				continue;
+			}
+
+			if (pair.getFirst().startsWith("1.3.6.1.4.1.7483.2.1.2.2.1.1.1.9")) {
+				data = pair.getSecond();
+				continue;
+			}
+
+			if (pair.getFirst().startsWith("1.3.6.1.4.1.7483.2.1.2.2.1.1.1.8")) {
+				changeObject = pair.getSecond();
+				flag = true;
+			}
+		}
+
+		if (flag) {
+			String key = ip + objectId + data + changeObject;
+			if (map.containsKey(key)) {
+				long oldTimeStamp = map.get(key);
+				if (timeStamp - oldTimeStamp > MAX_TIME) {
+					queue.push(trapInfo);
+				} 
+				map.put(key, timeStamp);
+			} else {
+				map.put(key, timeStamp);
+				queue.push(trapInfo);
+			}
+		}
+		log.debug("drop notification: " + trapInfo);
 	}
 }
